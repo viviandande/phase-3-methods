91d3be3aade20cb8dd1452eea42d5653db67df8b
+ <+>
2023-02-06 21:27:05 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+  end
+
+  def greet(name)
+    puts "Hello, #{name}!"
+  end
+
+  def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+  end
+
+  def add(num1, num2)
+    num1 + num2
+  end
+
+  def halve(number)
+    number.is_a?(Integer) ? number / 2 : nil
+  end
+
+
